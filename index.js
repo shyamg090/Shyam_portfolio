21947063e94dc448996366652699f5908a83d41b
unknown <samshyam9630@gmail.com>
2023-06-04 20:19:09 +0530

    animations and download link

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ const darkmodeFun = () => {
     console.log("hello");
 }
 
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const animation= document.querySelectorAll('.hidden');
+animation.forEach((div) => observer.observe(div));
+//passing each of the div with hidden to customObserver.observe()
+
+// https://drive.google.com/file/d/1-uP_WQPEHEijsOPtSJ4-WmYsHgyIuG8H/view?usp=share_link
+
+const downloadFun = ()=>{
+    window.open('https://drive.google.com/file/d/1-uP_WQPEHEijsOPtSJ4-WmYsHgyIuG8H/view?usp=share_link', '_blank');
+}
