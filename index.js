c2544bd5e7e018e64b137f22a66c19d544905803
unknown <samshyam9630@gmail.com>
2023-06-16 10:16:44 +0530

    bg-img added

--- index.js
+++ index.js
@@ -26,10 +26,23 @@ animation.forEach((div) => observer.observe(div));
 //passing each of the div with hidden to customObserver.observe()
 
 
-// ----------------------------download link for resume--------------------------------------
+// ----------------------------download link for resume + links(social media)--------------------------------------
 // https://drive.google.com/file/d/1-uP_WQPEHEijsOPtSJ4-WmYsHgyIuG8H/view?usp=share_link
 const downloadFun = ()=>{
     window.open('https://drive.google.com/file/d/1Z0xDsCFbdd-42yLqwRXU0nxMhq9mTALe/view?usp=sharing', '_blank');
+}
+
+const github =()=>{
+    window.open('https://github.com/shyamg090', '_blank');
+}
+const linkedin =()=>{
+    window.open('https://www.linkedin.com/in/shyam-g-6712a522b', '_blank');
+}
+const twitter =()=>{
+    window.open('https://twitter.com/_Shyam_G', '_blank');
+}
+const instagram =()=>{
+    window.open('https://www.instagram.com/shyam_g__/', '_blank');
 }
 
 
