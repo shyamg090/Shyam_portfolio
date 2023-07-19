71238bd369df3eba1c4bc6f7d96f96b5fda752d7
unknown <samshyam9630@gmail.com>
2023-07-19 08:02:42 +0530

    minor js changes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ animation.forEach((div) => observer.observe(div));
 // ----------------------------download link for resume + links(social media)--------------------------------------
 // https://drive.google.com/file/d/1-uP_WQPEHEijsOPtSJ4-WmYsHgyIuG8H/view?usp=share_link
 const downloadFun = ()=>{
-    window.open('https://drive.google.com/file/d/1KIgaBw-8B11UUg7Kmvb4pqW6Do40Fw6P/view?usp=drivesdk', '_blank');
+    window.open('https://drive.google.com/file/d/1LbbOrMDGsHSHi9E330VhLO8qAhKmAU4B/view?usp=sharing', '_blank');
 }
 
 const github =()=>{
@@ -45,6 +45,25 @@ const instagram =()=>{
     window.open('https://www.instagram.com/shyam_g__/', '_blank');
 }
 
+const project1 =()=>{
+    window.open('https://pokedex-by-shyam.netlify.app/','_blank');
+}
+const github1=()=>{
+    window.open('https://github.com/shyamg090/Pokedex-React-project','_blank');
+}
+
+const project2 =()=>{
+    window.open('https://foodcarti.netlify.app/','_blank');
+}
+const github2 =()=>{
+    window.open('https://github.com/shyamg090/foodcarti/','_blank');
+}
+const project3 =()=>{
+    window.open('https://todo-by-shyam.netlify.app/','_blank');
+}
+const github3 =()=>{
+    window.open('https://github.com/shyamg090/todo-react/','_blank');
+}
 
 // ---------------pop up ---------------------------------------------------
 const popupDiv= document.querySelector('.customPopup');
