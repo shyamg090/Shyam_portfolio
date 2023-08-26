77746ec36e4da90e10270bf586d3f91b131da29e
shyamg090 <shyamg3000@gmail.com>
2023-08-26 12:09:29 +0530

    added a api that fetch random api

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const darkmodeFun = () => {
   var mode;
 
   // ----------toggle and make it dark mode-------------------
-  document.querySelector(".lightmode").classList.toggle("darkmode");
+  document.querySelector(".darkmode").classList.toggle("lightmode");
 
   let navlinks = document.querySelectorAll(".navlinks");
   for (let i = 0; i < navlinks.length; i++) {
-    navlinks[i].classList.toggle("darkmode");
+    navlinks[i].classList.toggle("lightmode");
   }
 
   // ----------check if is in darkmode class is present--------------------------
   // ----------if no add class darkmode to document ++++ if yes then removes-------
-  if (document.querySelector(".lightmode").classList.contains("darkmode")) {
-    // alert("dark")
-    mode = "dark";
-  } else {
+  if (document.querySelector(".darkmode").classList.contains("lightmode")) {
     // alert("light")
     mode = "light";
+  } else {
+    // alert("dark")
+    mode = "dark";
   }
   //-------------set the local storage by json.stringify to convert it into string--------------------
   // ------------it send to local storage as object, hence use this to convert to strings-----------
@@ -30,8 +30,8 @@ const darkmodeFun = () => {
 let getMode = JSON.parse(localStorage.getItem("pageMode"));
 // alert(getMode)
 
-if (getMode == "dark") {
-  document.body.classList.add("darkmode"); //important line to set darkmode
+if (getMode == "light") {
+  document.body.classList.add("lightmode"); //important line to set darkmode
 }
 
 // ----------------------------intersection observer animation--------------------------------------
@@ -104,3 +104,30 @@ window.onload = () => {
 const popupClicked = () => {
   popupDiv.style.display = "none";
 };
+
+
+const quote = fetch('https://type.fit/api/quotes').then( (res) => res.json() ).then( (data) => {
+  console.log(data);
+  let random = 0; 
+  random = Math.floor((Math.random() * 15) + 1);
+
+  const quoteLine = document.querySelector('.quote-line');
+  quoteLine.innerHTML = data[random]?.text;
+
+  const author = document.querySelector('.author');
+  const authorname = data[random]?.author;
+  const n = authorname.length;
+
+  n>11 ? author.innerHTML = authorname.slice(0, n - 10) : author.innerHTML = "Yogi Beera"
+  // console.log(authorname.slice(0, n - 10));
+  // author.innerHTML = authorname.slice(0, n - 10);
+})
+
+useEffect(() => {
+  random();
+  quote();
+
+  return () => {
+    console.log('error');
+  }
+}, [])
